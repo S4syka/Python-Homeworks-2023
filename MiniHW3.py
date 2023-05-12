9d83b91feab9418b41e461f3a908ca55cd5611f6
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-05-12 19:48:37 +0400

    Fixed MiniHW3

--- MiniHW3.py
+++ MiniHW3.py
@@ -1,4 +1,4 @@
 def sum_no_duplicates(l):
-    return sum({numbers for numbers in l})
+    return sum(number for number in l if l.count(number) == 1)
 
-print(sum_no_duplicates([1,2,3,4,5,5,5,5]))
+print(sum_no_duplicates([3,4,3,6]))
